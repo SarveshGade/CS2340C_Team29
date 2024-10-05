bafe48b8571f24d276dd9870913ec9ade854b0ab
Tanmay Gupta <tanmaygupta102@gmail.com>
2024-10-05 19:13:10 -0400

    Create MVVM structure

--- Project/app/src/main/java/com/example/sprintproject/view/LoginActivity.java
+++ Project/app/src/main/java/com/example/sprintproject/view/LoginActivity.java
@@ -1,6 +1,5 @@
-package com.example.sprintproject;
+package com.example.sprintproject.view;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,32 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import com.example.sprintproject.R;
 
-public class splashActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(splashActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        },1500);
     }
 }
--- Project/app/src/main/java/com/example/sprintproject/view/MainActivity.java
+++ Project/app/src/main/java/com/example/sprintproject/view/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.sprintproject;
+package com.example.sprintproject.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.sprintproject.R;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -19,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, loginActivity.class);
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
