9dd95196589a239a43c7a55195c2bd67ef5e2047
SarveshGade <sarveshgade425@gmail.com>
2024-10-06 16:23:06 -0400

    Login and Activity

--- Project/app/src/main/java/com/example/sprintproject/view/LoginActivity.java
+++ Project/app/src/main/java/com/example/sprintproject/view/LoginActivity.java
@@ -1,16 +1,34 @@
 package com.example.sprintproject.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.sprintproject.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextInputEditText editTextUser, editTextPassword;
+    Button logButton;
+    FirebaseAuth mAuth;
+    ProgressBar progressBar;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +40,47 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        mAuth = FirebaseAuth.getInstance();
+        editTextUser = findViewById(R.id.user);
+        editTextPassword = findViewById(R.id.password);
+        logButton = findViewById(R.id.login_button);
+        progressBar = findViewById(R.id.progress_bar);
+        textView = findViewById(R.id.register_now);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SplashActivity.class);
+                startActivity(intent);
+//                finish();
+            }
+        });
+
+        logButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                String user = String.valueOf(editTextUser.getText());
+                String password = String.valueOf(editTextPassword.getText());
+
+                mAuth.signInWithEmailAndPassword(user, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(LoginActivity.this, "Logged In!",
+                                            Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                                progressBar.setVisibility(View.GONE);
+                            }
+                        });
+            }
+        });
     }
+
+
 }
