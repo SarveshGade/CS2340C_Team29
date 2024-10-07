a659e4f6e48141ffe5ca5fd6bb60e6d5e1c37a56
uiu252 <usmanrashid.7865@gmail.com>
2024-10-06 23:28:39 -0400

    Finally done with Navbar and Pages

---
+++ Project/app/src/main/java/com/example/sprintproject/view/DiningActivity.java
@@ -0,0 +1,79 @@
+package com.example.sprintproject.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.sprintproject.R;
+
+public class DiningActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_dining);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+
+
+
+        ImageButton logisticsButton = findViewById(R.id.logisticsButton);
+        ImageButton destinationButton = findViewById(R.id.destinationButton);
+        ImageButton diningButton = findViewById(R.id.diningButton);
+        ImageButton accommodationsButton = findViewById(R.id.accommodationsButton);
+        ImageButton communityButton = findViewById(R.id.communityButton);
+
+        logisticsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DiningActivity.this, LogisticsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        destinationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DiningActivity.this, LocationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        diningButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DiningActivity.this, DiningActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        accommodationsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DiningActivity.this, AccommodationsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        communityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DiningActivity.this, ForumActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
