fef97a61f4b86244c761dd362da9af0e15ec4ddd
SarveshGade <sarveshgade425@gmail.com>
2024-11-03 19:45:08 -0500

    Completed code smells 2, 3, 4

--- Code_Smells/TaxableItem.java
+++ Code_Smells/TaxableItem.java
@@ -2,8 +2,12 @@
 public class TaxableItem extends Item {
     private double taxRate = 7;
     
-    public TaxableItem(String name, double price, int quantity, DiscountType discountType, double discountAmount){
-        super(name, price, quantity, discountType, discountAmount);
+    public TaxableItem(Item toTax){
+        this.setName(toTax.getName());
+        this.setPrice(toTax.getPrice());
+        this.setQuantity(toTax.getQuantity());
+        this.setDiscountType(toTax.getDiscountType());
+        this.setDiscountAmount(toTax.getDiscountAmount());
     }
 
     public double getTaxRate(){
